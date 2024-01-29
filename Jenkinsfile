99c7c80d1831b48a27426f3314bdaaa17bbc8e66
Caio Henrique <caiohenrks@outlook.com>
2024-01-28 23:19:22 -0300

    first commit

---
+++ Jenkinsfile
@@ -0,0 +1,52 @@
+import groovy.json.JsonSlurper
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }  
+        
+        stage('SonarQube analysis') {
+            steps {
+                sh '/opt/sonar-scanner/bin/sonar-scanner -Dsonar.projectKey=QRCODE-API -Dsonar.sources=. -Dsonar.host.url=http://dockerlab:9000 -Dsonar.login=squ_924168670f71ab32440fda9ced08771703722cfd'
+            }
+        }
+
+        stage('Docker build') {
+            steps {
+                sh 'docker rm -f qrcodeapi || true'
+                sh 'docker rmi -f qrcodeapi || true'
+                sh 'docker build -t qrcodeapi .'
+                sh 'docker run -d -p 5000:5000 --name qrcodeapi qrcodeapi'
+            }
+        }
+
+        stage('Push Nexus') {
+        steps {
+            script {
+                def nexusUrl = '192.168.15.80:8082'
+                def repoName = 'docker'
+    
+                withCredentials([usernamePassword(credentialsId: 'NexusLogin', usernameVariable: 'NEXUS_USERNAME', passwordVariable: 'NEXUS_PASSWORD')]) {
+                    sh "docker login -u $NEXUS_USERNAME -p $NEXUS_PASSWORD ${nexusUrl}"
+                    sh "docker tag qrcodeapi ${nexusUrl}/${repoName}:latest"
+                    sh "docker push ${nexusUrl}/${repoName}:latest"
+                }
+            }
+        }
+    }
+
+
+        stage('Test Endpoints') {
+            steps {
+                sleep(time: 10, unit: 'SECONDS')
+                sh 'sudo chmod +x test_endpoints.sh'
+                sh './test_endpoints.sh'
+            }
+        }
+    }
+}
---
+++ test_endpoints.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Endereço do servidor
+SERVER_URL="http://192.168.15.80:5000"
+
+# Endpoint Register
+REGISTER_ENDPOINT="$SERVER_URL/register"
+
+# Endpoint Generate QRCode
+QRCODE_ENDPOINT="$SERVER_URL/generate_qrcode"
+
+# Solicitação ao endpoint Register
+response=$(curl -s -o - -w "\n%{http_code}" -H "Content-Type: application/json" -X POST -d '{"username": "testUser", "password": "testPass"}' $REGISTER_ENDPOINT)
+
+http_code=$(echo "$response" | tail -n1)
+response_body=$(echo "$response" | head -n -1)
+
+# Imprime a resposta do endpoint Register
+echo "===== RESPONSE FROM /register ====="
+echo "HTTP Code: $http_code"
+echo "Response Body:"
+echo "$response_body"
+echo "==================================="
+
+if [[ $http_code -ne 200 ]]; then
+    echo "Failed to register. Response code: $http_code"
+    exit 1
+fi
+
+# Extrai a api_key da resposta
+api_key=$(echo "$response_body" | jq -r '.api_key')
+
+if [[ -z "$api_key" ]]; then
+    echo "Failed to extract api_key from response."
+    exit 1
+fi
+
+# Solicitação ao endpoint Generate QRCode
+response=$(curl -s -o - -w "\n%{http_code}" -H "Content-Type: application/json" -H "x-api-key: $api_key" -X POST -d '{"content": "www.linkedin.com/in/caiohenrks"}' $QRCODE_ENDPOINT)
+
+http_code=$(echo "$response" | tail -n1)
+response_body=$(echo "$response" | head -n -1)
+
+# Imprime a resposta do endpoint Generate QRCode
+echo "===== RESPONSE FROM /generate_qrcode ====="
+echo "HTTP Code: $http_code"
+echo "Response Body:"
+# echo "$response_body"
+echo "=========================================="
+
+if [[ $http_code -ne 200 && $http_code -ne 401 ]]; then
+    echo "Failed to generate QRCode. Response code: $http_code"
+    exit 1
+fi
+
+
+echo "Script completed successfully!"
